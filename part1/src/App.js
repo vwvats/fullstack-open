25bc151dd733bd1c29b873888ef0e9c7faacfcca
VW Vats <vwvats@gmail.com>
2022-04-26 03:33:26 +0530

    exercise 1.1 and 1.2

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,27 +1,25 @@
+import Header from "./Header";
+import Content from "./Content";
+import Total from "./Total";
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  const parts = { part1, exercises1, part2, exercises2, part3, exercises3 };
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content {...parts} />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
