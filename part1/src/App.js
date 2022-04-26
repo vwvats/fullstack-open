b624d88072715d81ebd26e3363c7567c5a1bfae1
VW Vats <vwvats@gmail.com>
2022-04-26 12:27:04 +0530

    exercises 1.6 and 1.7

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,33 +1,41 @@
+import { useState } from "react";
 import Header from "./Header";
 import Content from "./Content";
 import Total from "./Total";
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      }
-    ]
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClick = (type) => {
+    switch(type) {
+      case 'good':
+        return () => setGood(good + 1);
+      case 'neutral':
+        return () => setNeutral(neutral + 1);
+      case 'bad':
+        return () => setBad(bad + 1);
+      default:
+        break;
+    }
   }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total exercises={course.parts} />
+     <h2>give feedback</h2>
+     <button onClick={handleClick('good')}>Good</button>
+     <button onClick={handleClick('neutral')}>Neutral</button>
+     <button onClick={handleClick('bad')}>Bad</button>
+     <h2>statistics</h2>
+     <p>good {good}</p>
+     <p>neutral {neutral}</p>
+     <p>bad {bad}</p>
+     <p>all {good + neutral + bad}</p>
+     <p>average {(good * 1 + bad * -1) / (good + neutral + bad)}</p>
+     <p>positive {good / (good + neutral + bad)}%</p>
     </div>
-  );
+  )
 };
 
 export default App;
