415aba871c951c8c23826e14a07d169bff1f1d0d
VW Vats <vwvats@gmail.com>
2022-04-26 13:01:22 +0530

    exercise 1.11

--- part1/src/Statistics.jsx
+++ part1/src/Statistics.jsx
@@ -1,18 +1,31 @@
-const StatisticLine = ({text, value}) => <p>{text} {value}</p>
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <th>{text}</th>
+    <td>{value}</td>
+  </tr>
+);
 
 const Statistics = ({ good, neutral, bad }) => {
   return (
     <>
       <h2>statistics</h2>
       {good > 0 || neutral > 0 || bad > 0 ? (
-        <div>
-          <StatisticLine text="good" value={good} />
-          <StatisticLine text="neutral" value={neutral} />
-          <StatisticLine text="bad" value={bad} />
-          <StatisticLine text="all" value={good + neutral + bad} />
-          <StatisticLine text="average" value={(good * 1 + bad * -1) / (good + neutral + bad)} />
-          <StatisticLine text="positive" value={`${good / (good + neutral + bad) * 100}%`} />
-        </div>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={good + neutral + bad} />
+            <StatisticLine
+              text="average"
+              value={(good * 1 + bad * -1) / (good + neutral + bad)}
+            />
+            <StatisticLine
+              text="positive"
+              value={`${(good / (good + neutral + bad)) * 100}%`}
+            />
+          </tbody>
+        </table>
       ) : (
         <p>No feedback given</p>
       )}
