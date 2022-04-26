b824fdd85d07d89044a285c6e68f001dbdb34aed
VW Vats <vwvats@gmail.com>
2022-04-26 14:57:28 +0530

    exercises 2.2 to 2.5

--- part2/src/Course.jsx
+++ part2/src/Course.jsx
@@ -1,14 +1,18 @@
-const Course = ({ course }) => {
-  return (
-    <>
+const Course = ({ courses }) => {
+  return courses.map((course) => {
+    return <div>
       <h1>{course.name}</h1>
       {course.parts.map((part) => (
         <p>
           {part.name} {part.exercises}
         </p>
       ))}
-    </>
-  );
+      <p>
+        total of {course.parts.reduce((acc, curr) => acc + curr.exercises, 0)}{" "}
+        exercises
+      </p>
+    </div>;
+  });
 };
 
 export default Course;
