b931ec38e57ef72295741e9d4f92671f6f89601c
VW Vats <vwvats@gmail.com>
2022-04-27 12:38:30 +0530

    exercise 3.2

--- part3/index.js
+++ part3/index.js
@@ -4,6 +4,16 @@ const phonebook = require("./data.js");
 const app = express();
 const PORT = 3001;
 
+app.get("/info", (request, response) => {
+  const numberOfPeople = phonebook.length;
+  const time = new Date();
+  const responseString = `
+    Phonebook has info for ${numberOfPeople} people 
+    ${time.toString()}
+  `;
+  response.send(responseString);
+});
+
 app.get("/api/persons", (request, response) => {
   response.json(phonebook);
 });
