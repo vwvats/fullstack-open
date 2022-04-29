87132f6324ab251fd62e8e7b0c5f6464d0132bc2
VW Vats <vwvats@gmail.com>
2022-04-29 15:17:20 +0530

    exercise 3.13 and 3.14 done

--- part3/index.js
+++ part3/index.js
@@ -1,32 +1,41 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-const cors = require('cors');
-
-let phonebook = require("./data.js");
+const cors = require("cors");
+const Contact = require("./models/contact");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.use(express.json());
-morgan.token('requestBody', (req, res) => JSON.stringify(req.body));
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :requestBody"));
+morgan.token("requestBody", (req, res) => JSON.stringify(req.body));
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :requestBody"
+  )
+);
 app.use(cors());
-app.use(express.static('build'));
+app.use(express.static("build"));
 
 app.get("/info", (request, response) => {
-  const numberOfPeople = phonebook.length;
-  const time = new Date();
-  const responseString = `
-    Phonebook has info for ${numberOfPeople} people 
-    ${time.toString()}
-  `;
-  response.send(responseString);
+  Contact.find({}).then((result) => {
+    const time = new Date();
+    const responseString = `
+      Phonebook has info for ${result.length} people 
+      ${time.toString()}
+    `;
+    response.send(responseString);
+  });
 });
 
+// fetch all contacts from DB
 app.get("/api/persons", (request, response) => {
-  response.json(phonebook);
+  Contact.find({}).then((result) => {
+    response.json(result);
+  });
 });
 
+// add a contact to DB (including duplicates)
 app.post("/api/persons", (request, response) => {
   const body = request.body;
   if (!body.name || !body.number) {
@@ -34,30 +43,31 @@ app.post("/api/persons", (request, response) => {
       error: "content missing",
     });
   }
-  if (phonebook.filter(contact => contact.name === body.name).length > 0) {
-    return response.status(400).json({
-      error: "name already in the phonebook",
-    });
-  }
-  const contact = { ...body, id: Math.random() * Math.random() };
-  phonebook = phonebook.concat(contact);
-  response.json(contact);
+  const contact = new Contact({ ...body });
+  contact.save().then((savedContact) => {
+    response.json(savedContact);
+  });
+  // IMPLEMENT LATER - check whether contact exists
+  // if (phonebook.filter((contact) => contact.name === body.name).length > 0) {
+  //   return response.status(400).json({
+  //     error: "name already in the phonebook",
+  //   });
+  // }
 });
 
+// fetch a contact by ID from DB
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const contact = phonebook.find((contact) => contact.id === id);
-  if (contact) {
+  Contact.findById(request.params.id).then((contact) => {
     response.json(contact);
-  } else {
-    response.send(`The id ${id} did not match any contact.`);
-  }
+  });
 });
 
+// delete a contact from DB
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  phonebook = phonebook.filter((note) => note.id !== id);
-  response.status(204).end();
+  Contact.deleteOne({ _id: Object(request.params.id) }).then((result) => {
+    console.log(result);
+    response.status(204).end();
+  });
 });
 
 app.listen(PORT, () => {
