96bb8e103d0a9c1cfca7ed85f069b86fe8da6902
VW Vats <vwvats@gmail.com>
2022-04-29 18:43:35 +0530

    exercise 3.20 - added custom validator to phone number

--- part3/models/contact.js
+++ part3/models/contact.js
@@ -16,11 +16,18 @@ mongoose
 const contactSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: 3,
+    minLength: 3,
     required: true,
   },
   number: {
     type: String,
+    minLength: 8,
+    validate: {
+      validator: function(phoneNum) {
+        return /^\d{2,3}-\d+$/.test(phoneNum);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
     required: true,
   },
 });
