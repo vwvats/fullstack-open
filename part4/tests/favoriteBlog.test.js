a285269a209ff3c6f02195fc1ffa69f14f42a159
VW Vats <vwvats@gmail.com>
2022-04-30 14:00:21 +0530

    modified dummy data and tests to exclude IDs

--- part4/tests/favoriteBlog.test.js
+++ part4/tests/favoriteBlog.test.js
@@ -5,24 +5,20 @@ describe('favorite blog', () => {
   test('for a list with a single blog, it returns the blog', () => {
     const result = listHelper.favoriteBlog(listWithOneBlog)
     expect(result).toEqual({
-      _id: '5a422aa71b54a676234d17f8',
       title: 'Go To Statement Considered Harmful',
       author: 'Edsger W. Dijkstra',
       url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
       likes: 5,
-      __v: 0
     })
   })
 
   test('for a list with many blogs, returns the blog with max likes', () => {
     const result = listHelper.favoriteBlog(listWithManyBlogs)
     expect(result).toEqual({
-      _id: '5a422b3a1b54a676234d17f9',
       title: 'Canonical string reduction',
       author: 'Edsger W. Dijkstra',
       url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
       likes: 12,
-      __v: 0
     })
   })
 })
