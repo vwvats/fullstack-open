9fcc41bd34c2c0205b5609b298a3ea3e09e7dff3
VW Vats <vwvats@gmail.com>
2022-04-30 14:58:07 +0530

    modified base route address

--- part4/app.js
+++ part4/app.js
@@ -18,6 +18,6 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-app.use('/api/blog', blogRouter)
+app.use('/api/blogs', blogRouter)
 
 module.exports = app
