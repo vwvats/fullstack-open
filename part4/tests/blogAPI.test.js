a6728e5b615fa41d340fe944d19900e2155e3dca
VW Vats <vwvats@gmail.com>
2022-04-30 21:50:55 +0530

    exercise 4.10

--- part4/tests/blogAPI.test.js
+++ part4/tests/blogAPI.test.js
@@ -31,4 +31,25 @@ test('each blog has an id property', async () => {
     })
 })
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'An eye catching Title',
+    author: 'Renowed Author',
+    url: 'https://google.com',
+    likes: 69
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const titles = response.body.map(r => r.title)
+
+  expect(response.body).toHaveLength(listWithManyBlogs.length + 1)
+  expect(titles).toContain('An eye catching Title')
+})
+
 afterAll(() => mongoose.connection.close())
